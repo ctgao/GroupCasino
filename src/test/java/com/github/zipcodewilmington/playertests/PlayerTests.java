99b8e05f52ba146cd4494171bb5a42a27224f810
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-19 16:22:31 -0400

    Add constructor

---
+++ src/test/java/com/github/zipcodewilmington/playertests/PlayerTests.java
@@ -0,0 +1,29 @@
+package com.github.zipcodewilmington.playertests;
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.player.PlayerClass;
+import org.junit.Test;
+
+import java.util.Calendar;
+
+public class PlayerTests {
+
+
+    @Test
+    public void testWallet () {
+        //Given
+        CasinoAccount wallet = new CasinoAccount();
+
+
+        //When
+
+        //Then
+
+    }
+
+
+
+
+
+
+
+}
