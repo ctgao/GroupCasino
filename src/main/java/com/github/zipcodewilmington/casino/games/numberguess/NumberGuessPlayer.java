912125cf9587375fb6068996bf154c602e764314
oahmed105 <oahmed10598@gmail.com>
2023-07-20 17:00:26 -0400

    commit

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayer.java
@@ -3,5 +3,36 @@ package com.github.zipcodewilmington.casino.games.numberguess;
 /**
  * Created by leon on 7/21/2020.
  */
-public class NumberGuessPlayer {
-}
+//public class NumberGuessPlayer {
+//
+//    private int playerScore = 0;
+//    private int totalGames;
+//
+//    public void setPlayerScore(int playerScore) {
+//        this.playerScore = playerScore;
+//    }
+//
+//    public void setTotalGames(int totalGames) {
+//        this.totalGames = totalGames;
+//    }
+//
+//    public void incrementScore() {
+//        playerScore++;
+//    }
+//
+//    public int getScore() {
+//        return playerScore;}
+//
+//    public double calcWinRate(int totalGames) {
+//        double winRate = (double) 100 * ((double) playerScore / totalGames);
+//        return winRate;
+//    }
+//
+//    public void updateGame(boolean win) {
+//
+//    }
+//
+//    public void displayPlayerInfo() {}
+//
+//
+//}
