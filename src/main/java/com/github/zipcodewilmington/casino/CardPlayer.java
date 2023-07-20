3ab94d56cd2f882b7e74be8db1d41d06e1ee4a35
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-07-20 15:33:36 -0400

    finished typing up card player

--- src/main/java/com/github/zipcodewilmington/casino/CardPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/CardPlayer.java
@@ -1,6 +1,7 @@
 package com.github.zipcodewilmington.casino;
 
 import com.github.zipcodewilmington.casino.cardutils.HandOfCards;
+import com.github.zipcodewilmington.casino.cardutils.PlayingCard;
 import com.github.zipcodewilmington.utils.IOConsole;
 
 import java.util.Scanner;
@@ -14,5 +15,32 @@ public abstract class CardPlayer extends PlayerClass{
         super(wallet, console);
     }
 
-    //
+    // my other functions
+    public HandOfCards getHandOfCards(){
+        return curHand;
+    }
+
+    public void receiveCard(PlayingCard pc){
+        curHand.addCard(pc);
+    }
+
+    public void useCard(PlayingCard pc){
+        curHand.removeCard(pc);
+    }
+
+    public void printHand(){
+        // not sure what to put here yet so i'll leave it empty for now
+    }
+
+    public void clearHand(){
+        curHand.clear();
+    }
+
+    public PlayingCard promptForCard(String prompt){
+        // not sure what to put here yet so i'll leave it empty for now
+        return null;
+    }
+
+    // abstract functions here
+    public abstract void sortHand();
 }
