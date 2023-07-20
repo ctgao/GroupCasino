630eb41a45e3a39ccb6246f81138ef6262f24797
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-07-19 21:20:43 -0400

    Hand of cards is complete and tested

--- src/main/java/com/github/zipcodewilmington/casino/cardutils/HandOfCards.java
+++ src/main/java/com/github/zipcodewilmington/casino/cardutils/HandOfCards.java
@@ -8,11 +8,14 @@ public class HandOfCards extends ArrayList<PlayingCard> {
         super();
     }
 
+    /*
+     * Note: not sure how necessary these two functions are since the superclass,
+     * ArrayList already has these as methods
+     */
     // adds a card to your hand
     public void addCard(PlayingCard pc){
         this.add(pc);
     }
-
     // removes a card from your hand
     public void removeCard(PlayingCard pc){
         this.remove(pc);
@@ -20,11 +23,22 @@ public class HandOfCards extends ArrayList<PlayingCard> {
 
     // checks for a specific value
     public int howManyOfValue(PlayingCardValue targetValue){
-        return 0;
+        int count = 0;
+        for(PlayingCard pc : this){
+            if(targetValue.equals(pc.getValue())){
+                count++;
+            }
+        }
+        return count;
     }
 
     // checks for a specific suit
     public boolean containsSuit(PlayingCardSuit targetSuit){
+        for(PlayingCard pc : this){
+            if(targetSuit.equals(pc.getSuit())){
+                return true;
+            }
+        }
         return false;
     }
 }
