5e062cc072325f8cf88048b14fc6b4ce516fc7b8
oahmed105 <oahmed10598@gmail.com>
2023-07-20 17:47:03 -0400

    NumberGuessPlayer and NumberGuessPlayer tests completed

--- src/test/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayerTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayerTest.java
@@ -6,68 +6,78 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//class NumberGuessPlayerTest {
-//
-//    @Test
-//    void incrementScoreTest() {
-//        //Given
-//        NumberGuessPlayer player = new NumberGuessPlayer();
-//        player.incrementScore();
-//        player.incrementScore();
-//        player.incrementScore();
-//        player.incrementScore();
-//        player.incrementScore();
-//
-//        //When
-//        int expected = player.getScore();
-//        int actual = 5;
-//
-//        //Then
-//        Assert.assertEquals(expected, actual);
-//
-//    }
-//
-//    @Test
-//    void getScoreTest() {
-//        //Given
-//        NumberGuessPlayer player = new NumberGuessPlayer();
-//
-//        //When
-//        int expected = player.getScore();
-//        int actual = 0;
-//
-//        //Then
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    void calcWinRateTest() {
-//        //Given
-//        NumberGuessPlayer player = new NumberGuessPlayer();
-//        player.setPlayerScore(34);
-//
-//        //When
-//        double expected = player.calcWinRate(100);
-//        double actual = 34.00;
-//
-//        //Then
-//        Assert.assertEquals(expected, actual, 0);
-//    }
-//
-//    @Test
-//    void updateGameTest() {
-//        //Given
-//        NumberGuessPlayer player = new NumberGuessPlayer();
-//        player.incrementScore();
-//        //When
-////        boolean expected = player.updateGame();
-//        //Then
-//    }
-//
-//    @Test
-//    void displayPlayerInfoTest() {
-//        //Given
-//        //When
-//        //Then
-//    }
-//}
+class NumberGuessPlayerTest {
+
+    @Test
+    void incrementScoreTest() {
+        //Given
+        NumberGuessPlayer player = new NumberGuessPlayer();
+        player.incrementScore();
+        player.incrementScore();
+        player.incrementScore();
+        player.incrementScore();
+        player.incrementScore();
+
+        //When
+        int expected = player.getScore();
+        int actual = 5;
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void setPlayerScore() {
+        //Given
+        NumberGuessPlayer player = new NumberGuessPlayer();
+        player.setPlayerScore(76);
+        //When
+        int expected = player.getScore();
+        int actual = 76;
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    void getScoreTest() {
+        //Given
+        NumberGuessPlayer player = new NumberGuessPlayer();
+
+        //When
+        int expected = player.getScore();
+        int actual = 0;
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    void calcWinRateTest() {
+        //Given
+        NumberGuessPlayer player = new NumberGuessPlayer();
+        player.setPlayerScore(34);
+
+        //When
+        double expected = player.calcWinRate(100);
+        double actual = 34.00;
+
+        //Then
+        Assert.assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    void updateGameTest() {
+        //Given
+        NumberGuessPlayer player = new NumberGuessPlayer();
+        player.setTotalGames(5);
+        //When
+        player.updateGame(true);
+        int expected = 6;
+        int actual = player.getTotalGames();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
+}
