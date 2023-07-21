1d58bcbea5e7b97868dac7a1efc30dc66e75f29b
oahmed105 <oahmed10598@gmail.com>
2023-07-20 23:08:56 -0400

    Finished one thing but added many more problems

---
+++ src/test/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGameTest.java
@@ -0,0 +1,37 @@
+package com.github.zipcodewilmington.casino.games.numberguess;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberGuessGameTest {
+
+    @Test
+    void randomNum() {
+        //Given
+
+        //When
+        //Then
+    }
+
+    @Test
+    void makeGuess() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    void validateGuess() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    void getRemainingGuesses() {
+        //Given
+        //When
+        //Then
+    }
+}
