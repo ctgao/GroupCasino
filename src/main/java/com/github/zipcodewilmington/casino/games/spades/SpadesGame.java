38534f42430bec733daf4ab57c6f8f7ccbdf2584
Christine <christine@zipcodersMBP13.localdomain>
2023-07-22 17:12:16 -0400

    wrote some stuff - but i wanna rewrite player choice things

--- src/main/java/com/github/zipcodewilmington/casino/games/spades/SpadesGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/spades/SpadesGame.java
@@ -1,4 +1,129 @@
 package com.github.zipcodewilmington.casino.games.spades;
 
-public class SpadesGame {
+import com.github.zipcodewilmington.casino.CardGame;
+import com.github.zipcodewilmington.casino.CardPlayer;
+import com.github.zipcodewilmington.casino.cardutils.PlayingCard;
+import com.github.zipcodewilmington.casino.cardutils.PlayingCardSuit;
+
+import javax.smartcardio.Card;
+
+public class SpadesGame extends CardGame {
+    boolean spadesBroken;
+    int[] playerOrder;
+
+    public SpadesGame(){
+        super();
+        // TRY TO CHANGE HOW THE PLAYER ARRAY LIST IS INSTANTIATED PER CARD PLAYER CLASS TO SEE IF IT'LL BE EASIER THAT WAY
+        spadesBroken = false;
+    }
+
+    @Override
+    public void run() {
+        // set the player order first
+        playerOrder = new int[getPlayers().size()];
+        setPlayerOrder(0);
+
+        // deal the cards to all players
+        // temporarily hard coded?
+        int handSize = 13;
+        dealCards(handSize);
+        // have all players sort their hands first
+        forceSort();
+
+        // play a round until everyone's hands are empty
+        while(!isEndCondition()){
+            // play through a round
+            PlayingCard[] curCards = playRound();
+            // get the winner
+            int indexOfWinningPlayer = getRoundWinner(curCards);
+            SpadesPlayer winner = (SpadesPlayer) getPlayers().get(indexOfWinningPlayer);
+            // increase the score and set up for the next round
+            winner.takeATrick();
+            setPlayerOrder(indexOfWinningPlayer);
+        }
+        
+        // now print the winner!
+        // also don't forget to update their score
+        SpadesPlayer youWon = getOverallWinner();
+        distributeWins(youWon);
+        printWinner();
+        
+    }
+
+    private void forceSort() {
+        for(CardPlayer cp : getPlayers()){
+            SpadesPlayer player = (SpadesPlayer) cp;
+            player.play();
+        }
+    }
+
+    private SpadesPlayer getOverallWinner() {
+        int maxScore = Integer.MIN_VALUE;
+        int maxIndex = Integer.MIN_VALUE;
+        for(int i = 0; i < getPlayers().size(); i++) {
+            SpadesPlayer player = (SpadesPlayer) getPlayers().get(i);
+            int finalScore = player.getScore();
+            if(finalScore > maxScore){
+                maxScore = finalScore;
+                maxIndex = i;
+            }
+        }
+        return (SpadesPlayer) getPlayers().get(maxIndex);
+    }
+
+    private void distributeWins(SpadesPlayer youWon) {
+        for(CardPlayer cp : getPlayers()) {
+            SpadesPlayer player = (SpadesPlayer) cp;
+            player.incrementScore(player.equals(youWon));
+        }
+    }
+
+    private PlayingCard[] playRound() {
+        PlayingCard[] result = new PlayingCard[getPlayers().size()];
+        PlayingCardSuit leadingSuit = null;
+        int i = 0;
+
+        // iterate over all the players
+        for(CardPlayer cp : getPlayers()){
+            SpadesPlayer player = (SpadesPlayer) cp;
+            PlayingCard temp = player.play(leadingSuit, spadesBroken);
+            result[i++] = temp;
+            if(leadingSuit == null){
+                leadingSuit = temp.getSuit();
+            }
+        }
+        return result;
+    }
+
+    public int getRoundWinner(PlayingCard[] curCards){
+        PlayingCard maxPC = null;
+        int maxIndex = Integer.MIN_VALUE;
+        for(int i = 0; i < curCards.length; i++){
+            PlayingCard pc = curCards[i];
+            if(maxPC == null || maxPC.compareTo(pc) < 0){
+                maxPC = pc;
+                maxIndex = i;
+            }
+        }
+        return playerOrder[maxIndex];
+    }
+
+    private void setPlayerOrder(int firstPlayerIndex) {
+        int n = playerOrder.length;
+
+        for(int i = 0; i < n; i++){
+            playerOrder[i] = (firstPlayerIndex + i) % n;
+        }
+    }
+
+    @Override
+    public void printWinner() {
+        // currently not sure if this is necessary - so???
+        // might need to do some refactoring later
+    }
+
+    @Override
+    public boolean isEndCondition() {
+        return getPlayers().get(0).getHandOfCards().isEmpty();
+    }
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/spades/SpadesPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/spades/SpadesPlayer.java
@@ -5,8 +5,10 @@ import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.cardutils.HandOfCards;
 import com.github.zipcodewilmington.casino.cardutils.PlayingCard;
 import com.github.zipcodewilmington.casino.cardutils.PlayingCardSuit;
-import com.github.zipcodewilmington.casino.cardutils.PlayingCardValue;
 import com.github.zipcodewilmington.utils.IOConsole;
+
+import java.util.Collections;
+import java.util.Random;
 
 public class SpadesPlayer extends CardPlayer {
     int tricksTaken = 0;
@@ -34,24 +36,86 @@ public class SpadesPlayer extends CardPlayer {
 
     @Override
     public void sortHand() {
-//        HandOfCards hand = getHandOfCards();
-//        int curIndex = 0;
-//        // go through all the suits, and spades are last
-//        for(PlayingCardSuit suit : PlayingCardSuit.values()){
-//            int swapIndex = curIndex;
-//            while(swapIndex < hand.size()){
-//                // look for the card that matches the current suit
-//                if(hand.get(swapIndex).com)
-//            }
-//        }
+        Collections.sort(this.getHandOfCards());
     }
 
     @Override
-    public <SomeReturnType> String play() {
+    public <PlayingCard> PlayingCard play() {
+        sortHand();
         return null;
+    }
+
+    public PlayingCard play(PlayingCardSuit winningSuit, boolean canPlaySpades) {
+        if(humanPlayer){
+            printHand();
+            return humanChoice(winningSuit, canPlaySpades);
+        }
+        else{
+            return computerChoice(winningSuit, canPlaySpades);
+        }
+    }
+
+    private PlayingCard computerChoice(PlayingCardSuit winningSuit, boolean canPlaySpades) {
+        Random rand = new Random();
+        int cardIndex;
+        // no winning suit? just pick a card
+        if(winningSuit == null || getHandOfCards().containsSuit(winningSuit)){
+            cardIndex = rand.nextInt(getHandOfCards().size());
+        }
+        else{
+            // if you have the suit, pick a card in that range
+            // count the number of cards based on your current suit
+            int countSameSuit = 0;
+            // save the starting index of said suit
+            int startingIndexOfSuit = Integer.MAX_VALUE;
+            for(int i = 0; i < getHandOfCards().size(); i++){
+                PlayingCard pc = getHandOfCards().get(i);
+                if(pc.getSuit().equals(winningSuit)){
+                    countSameSuit++;
+                    if(startingIndexOfSuit == Integer.MAX_VALUE){
+                        // if the var only contains max value, then this is the start of this suit
+                        startingIndexOfSuit = i;
+                    }
+                }
+            }
+            cardIndex = selectCard(startingIndexOfSuit, countSameSuit, rand);
+        }
+        return getHandOfCards().get(cardIndex);
+    }
+
+    public int selectCard(int startingIndex, int totalCards, Random rand) {
+        if(totalCards == 0){
+            // NO CARDS!!!
+            return rand.nextInt(getHandOfCards().size());
+        }
+        // we have some of said suit
+        return rand.nextInt(totalCards) + startingIndex;
+    }
+
+    private PlayingCard humanChoice(PlayingCardSuit winningSuit, boolean canPlaySpades) {
+        PlayingCard input;
+        do {
+            input = this.promptForCard("Which card would you like to play?");
+            if(!validateCard(winningSuit, input)){
+                this.printToConsole("INVALID CHOICE! Please pick another card.");
+            }
+        } while(!validateCard(winningSuit, input));
+        return null;
+    }
+
+    public boolean validateCard(PlayingCardSuit winningSuit, PlayingCard input) {
+        // first check to see if the input card exists in your hand
+        if(getHandOfCards().cont)
     }
 
     public void takeATrick(){
         tricksTaken++;
+    }
+
+    public void incrementScore(boolean didYouWin){
+        if(didYouWin){
+            setTotalGamesWon(getTotalGamesWon() + 1);
+        }
+        setTotalGamesPlayed(getTotalGamesPlayed() + 1);
     }
 }
